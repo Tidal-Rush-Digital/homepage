c4e9da65c147823ad3e41e40ade27aacb519543b
zjones2142 <zachjones2142@gmail.com>
2025-02-12 16:58:21 -0600

    zj - deploying changes

--- docs/server/angular-app-engine-manifest.mjs
+++ docs/server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/WebsiteAngularSPA',
+  basePath: '/homepage',
   supportedLocales: {
   "en-US": ""
 },
--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -2,19 +2,19 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/WebsiteAngularSPA/',
+  baseHref: '/homepage/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/WebsiteAngularSPA"
+    "route": "/homepage"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 518, hash: '2b26bb5d028e4fa5c27aff338817ee4c01339cb6e793144b24d3078668a918e1', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1031, hash: 'eddb95cd23fe25ce83ba4ded1ea5fae480410fcb65f683c00cf655e5f0691f64', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 4035, hash: '0f82fc2d5ef1e368eb98bea97643d1bd208db132826be9fa39ead6ba2aedc8be', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 509, hash: '48f4f01461768be2d81d8987511045f28863659bb5a7cc1723f4ca3572904139', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1022, hash: 'f00a01c8d25c0010adeb273da9bde426bbeb43f7b48f36a4d210a0e85428fc82', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 4551, hash: '73f6f974386b52858a1e2842bda9c31175c97e2f6d94c6cf902ecac5d82fa604', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
